c8e89cec9ba96c0ce7f853385cde57704fa0aa03
Juho Alenius <juho.alenius@student.lab.fi>
2021-11-21 17:24:17 +0200

    removed nodemodules

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 morgan.token('postdata', (req, res) => JSON.stringify(req.body))
@@ -87,7 +90,7 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
